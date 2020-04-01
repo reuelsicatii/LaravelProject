9446098108b8c02dfc5979308b767087a97478e0
Reuel Sicat II <reuel@truelogic.com.ph>
2020-04-01 14:35:41 +0800

    LaravelFromScratch - Video03

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,26 @@
 <?php
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+ * |--------------------------------------------------------------------------
+ * | Web Routes
+ * |--------------------------------------------------------------------------
+ * |
+ * | Here is where you can register web routes for your application. These
+ * | routes are loaded by the RouteServiceProvider within a group which
+ * | contains the "web" middleware group. Now create something great!
+ * |
+ */
+
+
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/StaticRoute', function () {
+    return view('pages.page01');
+});
+
+    Route::get('/DynamicRoute/{name}', function ($name) {
+    return view('pages.page02')->with('name',$name);
 });
